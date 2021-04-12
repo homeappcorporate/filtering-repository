018b2d96cc6474344b0ff716a5ed210936ddcf2a
Vasil <kulakov@vasiliy.pro>
2021-04-12 12:10:19 +0300

    Добавил тест

---
+++ tests/Filter/DTO/EqualTest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Homeapp\Filter\DTO\Field;
+
+use PHPUnit\Framework\TestCase;
+
+class EqualTest extends TestCase
+{
+    /**
+     * @dataProvider values
+     *
+     * @param mixed $value
+     */
+    public function testGetValue($value): void
+    {
+        $field = new Equal('somefield', $value);
+        self::assertEquals($value, $field->getValue());
+    }
+
+    /**
+     * @dataProvider values
+     *
+     * @param mixed $value
+     * @param bool  $empty
+     */
+    public function testIsEmpty($value, bool $empty): void
+    {
+        $field = new Equal('somefield', $value);
+        self::assertEquals($empty, $field->isEmpty());
+    }
+
+    /**
+     * @dataProvider values
+     *
+     * @param mixed $value
+     * @param bool  $empty
+     * @param bool  $true
+     */
+    public function testIsTrue($value, bool $_, bool $true, bool $false): void
+    {
+        $field = new Equal('somefield', $value);
+        self::assertEquals($true, $field->isTrue());
+        self::assertEquals($false, $field->isFalse());
+    }
+
+    public function testGetName(): void
+    {
+        $field = new Equal('somefield', 1);
+        self::assertEquals('somefield', $field->getName());
+    }
+
+    public function values(): array
+    {
+        return [
+            ['value' => 1, 'empty' => false, 'true' => false, 'false' => false],
+            ['value' => '1', 'empty' => false, 'true' => false, 'false' => false],
+            ['value' => [1, 2, 3], 'empty' => false, 'true' => false, 'false' => false],
+            ['value' => 'true', 'empty' => false, 'true' => true, 'false' => false],
+            ['value' => true, 'empty' => false, 'true' => true, 'false' => false],
+            ['value' => 'false', 'empty' => false, 'true' => false, 'false' => true],
+            ['value' => false, 'empty' => false, 'true' => false, 'false' => true],
+            ['value' => null, 'empty' => true, 'true' => false, 'false' => false],
+            ['value' => '', 'empty' => true, 'true' => false, 'false' => false],
+        ];
+    }
+}
